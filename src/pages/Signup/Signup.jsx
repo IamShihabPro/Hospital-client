6397ccc4d209ec2b925bf6d0212d767ffc4a5913
IamShihabPro <mshihab.pro@gmail.com>
2024-02-29 01:20:20 +0600

    signup page

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import SignUp from '../../assets/images/signup.jpg'
+import UserIcon from '../../assets/images/User-Icon.png'
 const Signup = () => {
     const [formData, setFormData] = useState({
         name:'',
@@ -25,33 +26,73 @@ const Signup = () => {
             <div className="px-4">
                 <div className="max-w-6xl mx-auto">
                     <div className="grid grid-cols-1 lg:grid-cols-2">
-                        <div className="hidden lg:block bg-blue-500 rounded-l-lg">
-                            <img className="" src="https://img.freepik.com/free-vector/sign-up-concept-illustration_114360-7965.jpg?t=st=1709125652~exp=1709129252~hmac=d1cc252bd48a47a5d7fc630111ea2506a7e346605510ab49a3ed2475db19ac95&w=740" alt="" />
+                        <div className="hidden lg:block rounded-l-lg">
+                            <img className="" src={SignUp} alt="" />
 
                         </div>
 
                         <div className="w-full max-w-xl mx-auto shadow-lg md:p-10">
-                            <h3 className="text-xl font-bold text-center mb-6">Create An <span className="text-blue-500">Account</span> </h3>
+                            <h3 className="text-xl font-bold text-center mt-2 mb-6">Create An <span className="text-blue-500">Account</span> </h3>
 
                             <form onSubmit={handleSubmit} className="py-4 md:py-0 px-3">
                                 <div className="mb-5">
-                                    <input type="name" name="name" value={formData.name} onChange={handleInputChange} placeholder="Enter Your Name"
+                                    <input type="name" name="name" value={formData.name} onChange={handleInputChange} placeholder="Full Name"
                                     className="w-full px-4 py-3 border-b border-solid border-gray-600 focus:outline-none focus:border-b-blue-500 text-md leading-7 text-blue-500"
                                     />
                                 </div>
 
                                 <div className="mb-5">
-                                    <input type="email" name="email" value={formData.email} onChange={handleInputChange} placeholder="Enter Your Email"
+                                    <input type="email" name="email" value={formData.email} onChange={handleInputChange} placeholder="Your Email"
                                     className="w-full px-4 py-3 border-b border-solid border-gray-600 focus:outline-none focus:border-b-blue-500 text-md leading-7 text-blue-500"
                                     />
                                 </div>
                                 <div className="mb-5">
-                                    <input type="password" name="password" value={formData.password} onChange={handleInputChange} placeholder="Enter Your Password"
+                                    <input type="password" name="password" value={formData.password} onChange={handleInputChange} placeholder="Password"
                                     className="w-full px-4 py-3 border-b border-solid border-gray-600 focus:outline-none focus:border-b-blue-500 text-md leading-7 text-blue-500"
                                     />
                                 </div>
+
+                               <div className="flex flex-col md:flex-row lg:flex-row justify-between items-center mb-6">
+                                    <label className="font-semibold text-gray-700 leading-7">
+                                        Are you:
+                                        <select 
+                                        name="role" 
+                                        className="ml-2 focus:outline-none font-semibold text-gray-600 leading-7 px-2 py-2"
+                                        >
+                                        <option value="patient">Patient</option>
+                                        <option value="doctor">Doctor</option>
+                                        </select>
+                                    </label>
+
+                                    <label className="font-semibold text-gray-700 leading-7">
+                                        Gender:
+                                        <select 
+                                        name="gender" 
+                                        className="ml-2 focus:outline-none font-semibold text-gray-600 leading-7 px-2 py-2"
+                                        >
+                                        <option value="patient">Male</option>
+                                        <option value="doctor">Female</option>
+                                        </select>
+                                    </label>
+                                </div>
+
+                                <div className="my-2 flex items-center gap-3">
+                                    <div className="">
+                                        <figure className="w-10 h-10 rounded-full border-2 border-solid border-blue-600">
+                                                <img src={UserIcon} alt="" />
+                                        </figure>
+                                    </div>
+                                    <div className="relative w-32 h-12">
+                                        <input type="file" id="customFile" name="photo" accept=".jpg, .png" 
+                                        className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+                                        />
+                                        <label htmlFor="customFile" className="absolute top-0 left-0 w-full h-full flex items-center px-2  leading-6 overflow-hidden bg-blue-400 text-white font-semibold rounded-lg cursor-pointer truncate">Upload Image</label>
+                                    </div>
+                                </div>
+
+
                                 <div>
-                                    <button className="w-full bg-blue-500 px-4 py-2 text-white rounded-sm">Login</button>
+                                    <button className="w-full bg-blue-500 px-4 py-2 text-white rounded-sm mt-2">Sign Up</button>
                                 </div>
 
                                 <p className="text-gray-500 text-center mt-4">Already have an account? <Link className="ml-1 text-blue-500 font-bold" to='/login'> Login </Link> </p> 
